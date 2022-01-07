43c72580e48edd3c0db2691db38e3c9cd99a5a2e
Josh Brunner <brunnerj@mit.edu>
2022-01-06 21:36:43 -0500

    .

--- src/josh/Miner.java
+++ src/josh/Miner.java
@@ -15,138 +15,141 @@ public class Miner extends Robot {
         mine();
     }
     private void movement() throws GameActionException {
+        boolean[][] hasNearbyMiner = new boolean[9][9];
         RobotInfo[] nearby = rc.senseNearbyRobots(RobotType.MINER.visionRadiusSquared, rc.getTeam());
         RobotInfo nearest = null;
         for(RobotInfo r:nearby) {
-            if(r.type==RobotType.MINER && (nearest==null || rc.getLocation().distanceSquaredTo(nearest.location) > rc.getLocation().distanceSquaredTo(r.location)))
-                nearest = r;
+            if(r.type==RobotType.MINER) {
+                if(nearest==null || rc.getLocation().distanceSquaredTo(nearest.location) > rc.getLocation().distanceSquaredTo(r.location))
+                    nearest=r;
+                hasNearbyMiner[r.location.x+4-rc.getLocation().x][r.location.y+4-rc.getLocation().y] = true;
+            }
         }
         if(nearest!=null) {
-            moveInDirection(nearest.location.directionTo(rc.getLocation()));
-            return;
+            //moveInDirection(nearest.location.directionTo(rc.getLocation()));
+            //return;
         }
         MapLocation l = rc.getLocation();
         MapLocation loc;
-        
-        if(rc.canSenseLocation(loc=l.translate(-2, 0)) && rc.senseLead(loc)>1) {
+        if(rc.canSenseLocation(loc=l.translate(-2, 0)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][4]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(0, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(0, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[4][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(0, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(0, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[4][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, 0)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, 0)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][4]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, -1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, -1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][3]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, 1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, 1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][5]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-1, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-1, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[3][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-1, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-1, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[3][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(1, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(1, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[5][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(1, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(1, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[5][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, -1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, -1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][3]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, 1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, 1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][5]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, 0)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, 0)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][4]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(0, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(0, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[4][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(0, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(0, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[4][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, 0)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, 0)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][4]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, -1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, -1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][3]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, 1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, 1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][5]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-1, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-1, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[3][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-1, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-1, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[3][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(1, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(1, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[5][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(1, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(1, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[5][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, -1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, -1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][3]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, 1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, 1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][5]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-4, 0)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-4, 0)) && rc.senseLead(loc)>1 && !hasNearbyMiner[0][4]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(0, -4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(0, -4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[4][0]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(0, 4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(0, 4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[4][8]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(4, 0)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(4, 0)) && rc.senseLead(loc)>1 && !hasNearbyMiner[8][4]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-4, -1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-4, -1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[0][3]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-4, 1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-4, 1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[0][5]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-1, -4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-1, -4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[3][0]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-1, 4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-1, 4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[3][8]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(1, -4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(1, -4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[5][0]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(1, 4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(1, 4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[5][8]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(4, -1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(4, -1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[8][3]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(4, 1)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(4, 1)) && rc.senseLead(loc)>1 && !hasNearbyMiner[8][5]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-3, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-3, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[1][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, -3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, -3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][1]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(3, 3)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(3, 3)) && rc.senseLead(loc)>1 && !hasNearbyMiner[7][7]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-4, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-4, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[0][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-4, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-4, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[0][6]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, -4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, -4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][0]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(-2, 4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(-2, 4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[2][8]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, -4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, -4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][0]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(2, 4)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(2, 4)) && rc.senseLead(loc)>1 && !hasNearbyMiner[6][8]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(4, -2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(4, -2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[8][2]) {
             moveToward(loc);
-        } else if(rc.canSenseLocation(loc=l.translate(4, 2)) && rc.senseLead(loc)>1) {
+        } else if(rc.canSenseLocation(loc=l.translate(4, 2)) && rc.senseLead(loc)>1 && !hasNearbyMiner[8][6]) {
             moveToward(loc);
         } else {
             if(rc.isActionReady()) //only wander if you did not mine this turn
